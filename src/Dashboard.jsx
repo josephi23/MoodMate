e0291d2766036164f771a3d0d6dc6df4ccf54092
Chidiebere Joseph <joseph.onyeisi@centria.fi>
2025-04-25 16:16:28 +0300

    Implement dashboard for management, history and journal logging.

---
+++ src/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div>
+      <h2>DASHBOARD 🏠</h2>
+      <p>Welcome to MOODMATE: Track your moods and reflections here.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
